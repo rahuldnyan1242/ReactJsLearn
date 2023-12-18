05337632a10696d25e64da3b14fcb13c0f19cebc
SYNAPSE\rahul.shiwarkar <rahul.shiwarkar@verizonconnect.com>
2023-12-18 12:54:14 +0530

    Changes in navbar

--- reactcourse/src/components/Navbar.js
+++ reactcourse/src/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar(props) {
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-success" type="submit">Search</button>
           </form> */}
-          <div>
+          <div className='my-6'>
             {colorChangeOptions.map((colorOption, index)=>(
               <button
               key={index}
