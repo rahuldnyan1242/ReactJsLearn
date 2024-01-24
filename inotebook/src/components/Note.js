74dffa7fa91677c53b29748b8001c42f9b9d7dbf
SYNAPSE\rahul.shiwarkar <rahul.shiwarkar@verizonconnect.com>
2024-01-24 14:41:30 +0530

    Interview questions folder

--- inotebook/src/components/Note.js
+++ inotebook/src/components/Note.js
@@ -27,74 +27,74 @@ const Note = (props) => {
     })
 
 
-const updateNote = (currentNote) => {
-    ref.current.click();
-    setNote({
-        id: currentNote._id,
-        etitle: currentNote.title,
-        edescription: currentNote.description,
-        etag: currentNote.tag
-    });
-}
+    const updateNote = (currentNote) => {
+        ref.current.click();
+        setNote({
+            id: currentNote._id,
+            etitle: currentNote.title,
+            edescription: currentNote.description,
+            etag: currentNote.tag
+        });
+    }
 
-const handleUpdateNoteClick = (e) => {
-    e.preventDefault();
-    editNote(note.id, note.etitle, note.edescription, note.etag);
-    refClose.current.click();
-    props.showAlert("User note is updated successfully", "success");
-}
+    const handleUpdateNoteClick = (e) => {
+        e.preventDefault();
+        editNote(note.id, note.etitle, note.edescription, note.etag);
+        refClose.current.click();
+        props.showAlert("User note is updated successfully", "success");
+    }
 
-const onChange = (e) => {
-    setNote({ ...note, [e.target.name]: e.target.value })
-}
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value })
+    }
 
-return (
-    <>
-        <AddNote showAlert={showAlert} />
-        <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
-        </button>
+    return (
+        <>
+            <AddNote showAlert={showAlert} />
+            <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
 
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <form>
-                            <div className="mb-3">
-                                <label htmlFor="title" className="form-label">Title</label>
-                                <input type="text" className="form-control" id="etitle" name='etitle' value={note.etitle} aria-describedby="emailHelp" onChange={onChange} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="description" className="form-label">Description</label>
-                                <input type="text" className="form-control" id="edescription" name='edescription' value={note.edescription} onChange={onChange} />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="tag" className="form-label">Tag</label>
-                                <input type="text" className="form-control" id="etag" name='etag' value={note.etag} onChange={onChange} />
-                            </div>
-                            {/* <button type="submit" className="btn btn-primary" onClick={handleAddNoteClick}>Submit</button> */}
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary" onClick={handleUpdateNoteClick}>Update Note</button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                    <input type="text" className="form-control" id="etitle" name='etitle' value={note.etitle} aria-describedby="emailHelp" onChange={onChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <input type="text" className="form-control" id="edescription" name='edescription' value={note.edescription} onChange={onChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="tag" className="form-label">Tag</label>
+                                    <input type="text" className="form-control" id="etag" name='etag' value={note.etag} onChange={onChange} />
+                                </div>
+                                {/* <button type="submit" className="btn btn-primary" onClick={handleAddNoteClick}>Submit</button> */}
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={handleUpdateNoteClick}>Update Note</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <hr />
-        <div className="row my-3">
-            <h2>Your Notes</h2>
-            {notes.map((note) => {
-                return <Noteitem showAlert={showAlert} key={note._id} updateNote={updateNote} note={note} />
-            })}
-        </div>
-    </>
-)
+            <hr />
+            <div className="row my-3">
+                <h2>Your Notes</h2>
+                {notes.map((note) => {
+                    return <Noteitem showAlert={showAlert} key={note._id} updateNote={updateNote} note={note} />
+                })}
+            </div>
+        </>
+    )
 }
 
 export default Note
